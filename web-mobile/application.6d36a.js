542026949c5b541a18fa5eaacd6594254aca8b1c
Khoa Nguyen <khoadtnwin@gmail.com>
2025-04-17 10:19:41 +0700

    feat: Move build into a build folder

--- web-mobile/application.6d36a.js
+++ web-mobile/application.6d36a.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.387ef.json';
+          this.settingsPath = 'src/settings.1fcd1.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- web-mobile/index.46f60.js
+++ web-mobile/index.46f60.js
@@ -1,4 +1,4 @@
-System.register(["./application.dab63.js"], function (_export, _context) {
+System.register(["./application.6d36a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
